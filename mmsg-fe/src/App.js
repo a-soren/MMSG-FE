cefef3c8d01c8b90f41e5374b96272384604b013
Amber Sorensen <ambersorensen@ambers-mbp.lan>
2020-02-23 22:56:58 -0700

    added dropdown menu, submit button, and basic form for front end. React strap is installed and ready to start styling

--- mmsg-fe/src/App.js
+++ mmsg-fe/src/App.js
@@ -1,26 +1,108 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Dropdown } from 'react-bootstrap';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Converter">
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Base Currency
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item >USD</Dropdown.Item>
+          <Dropdown.Item >JPY</Dropdown.Item>
+          <Dropdown.Item >BGN</Dropdown.Item>
+          <Dropdown.Item >CZK</Dropdown.Item>
+          <Dropdown.Item >DKK</Dropdown.Item>
+          <Dropdown.Item >GBP</Dropdown.Item>
+          <Dropdown.Item >HUF</Dropdown.Item>
+          <Dropdown.Item >PLN</Dropdown.Item>
+          <Dropdown.Item >RON</Dropdown.Item>
+          <Dropdown.Item >SEK</Dropdown.Item>
+          <Dropdown.Item >CHF</Dropdown.Item>
+          <Dropdown.Item >ISK</Dropdown.Item>
+          <Dropdown.Item >NOK</Dropdown.Item>
+          <Dropdown.Item >HRK</Dropdown.Item>
+          <Dropdown.Item >RUB</Dropdown.Item>
+          <Dropdown.Item >TRY</Dropdown.Item>
+          <Dropdown.Item >AUD</Dropdown.Item>
+          <Dropdown.Item >BRL</Dropdown.Item>
+          <Dropdown.Item >CAD</Dropdown.Item>
+          <Dropdown.Item >CNY</Dropdown.Item>
+          <Dropdown.Item >HKD</Dropdown.Item>
+          <Dropdown.Item >IDR</Dropdown.Item>
+          <Dropdown.Item >ILS</Dropdown.Item>
+          <Dropdown.Item >INR</Dropdown.Item>
+          <Dropdown.Item >KRW</Dropdown.Item>
+          <Dropdown.Item >MXN</Dropdown.Item>
+          <Dropdown.Item >MYR</Dropdown.Item>
+          <Dropdown.Item >NZD</Dropdown.Item>
+          <Dropdown.Item >PHP</Dropdown.Item>
+          <Dropdown.Item >SGD</Dropdown.Item>
+          <Dropdown.Item >THB</Dropdown.Item>
+          <Dropdown.Item >ZAR</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
+      <form>
+        <input type='text'
+          placeholder='The amount you want to convert'
+          name='Base amount' />
+
+      </form>
+
+
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Target Currency
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item >USD</Dropdown.Item>
+          <Dropdown.Item >JPY</Dropdown.Item>
+          <Dropdown.Item >BGN</Dropdown.Item>
+          <Dropdown.Item >CZK</Dropdown.Item>
+          <Dropdown.Item >DKK</Dropdown.Item>
+          <Dropdown.Item >GBP</Dropdown.Item>
+          <Dropdown.Item >HUF</Dropdown.Item>
+          <Dropdown.Item >PLN</Dropdown.Item>
+          <Dropdown.Item >RON</Dropdown.Item>
+          <Dropdown.Item >SEK</Dropdown.Item>
+          <Dropdown.Item >CHF</Dropdown.Item>
+          <Dropdown.Item >ISK</Dropdown.Item>
+          <Dropdown.Item >NOK</Dropdown.Item>
+          <Dropdown.Item >HRK</Dropdown.Item>
+          <Dropdown.Item >RUB</Dropdown.Item>
+          <Dropdown.Item >TRY</Dropdown.Item>
+          <Dropdown.Item >AUD</Dropdown.Item>
+          <Dropdown.Item >BRL</Dropdown.Item>
+          <Dropdown.Item >CAD</Dropdown.Item>
+          <Dropdown.Item >CNY</Dropdown.Item>
+          <Dropdown.Item >HKD</Dropdown.Item>
+          <Dropdown.Item >IDR</Dropdown.Item>
+          <Dropdown.Item >ILS</Dropdown.Item>
+          <Dropdown.Item >INR</Dropdown.Item>
+          <Dropdown.Item >KRW</Dropdown.Item>
+          <Dropdown.Item >MXN</Dropdown.Item>
+          <Dropdown.Item >MYR</Dropdown.Item>
+          <Dropdown.Item >NZD</Dropdown.Item>
+          <Dropdown.Item >PHP</Dropdown.Item>
+          <Dropdown.Item >SGD</Dropdown.Item>
+          <Dropdown.Item >THB</Dropdown.Item>
+          <Dropdown.Item >ZAR</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
+      <button type='submit'>Submit</button>
+
     </div>
   );
 }
 
 export default App;
+
+
